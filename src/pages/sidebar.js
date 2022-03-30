13bcb7448b9f4783a49e3422d0a58c7b71ccd748
Vannent <erobced@gmail.com>
2022-03-30 17:16:13 -0500

    feat: add functionality to logout butt

--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -1,3 +1,5 @@
+import loadgetName from "../functions/getName";
+
 const sidebar = () => {
     const sidebar = document.createElement("div");
     sidebar.className = "sidebar";
@@ -33,7 +35,10 @@ const sidebar = () => {
         <svg style="width:24px;height:24px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M19,3H5C3.89,3 3,3.89 3,5V9H5V5H19V19H5V15H3V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10.08,15.58L11.5,17L16.5,12L11.5,7L10.08,8.41L12.67,11H3V13H12.67L10.08,15.58Z" /></svg> Log Out`;
         logoutButton.addEventListener("click", (e) => {
-            
+            const content = document.querySelector("#content");
+            content.removeChild(content.lastChild)
+            loadgetName();
+            localStorage.clear();
         });
 
 
